d21fb49633c6c881a284e57582bed2a5be4b0ce4
Jeffery-C <j63007@gmail.com>
2022-03-15 14:42:27 +0800

    520.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired
@@ -16,6 +16,14 @@ class MyForm(FlaskForm):
                              validators=[DataRequired(),
                                          validators.Length(min=8, message="Field must be at least 8 characters long.")])
     submit = SubmitField(label="Log In")
+
+
+def check_login(form: MyForm) -> bool:
+    if form.email.data != "admin@email.com":
+        return False
+    if form.password.data != "12345678":
+        return False
+    return True
 
 
 app = Flask(__name__)
@@ -37,7 +45,14 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = MyForm()
-    form.validate_on_submit()
+    if form.validate_on_submit():
+        print(form.email.data)
+        print(form.password.data)
+        print(form.submit.data)
+        if check_login(form):
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
 
     return render_template('login.html', form=form)
 
